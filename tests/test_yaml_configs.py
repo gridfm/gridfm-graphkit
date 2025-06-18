09d33809679bd06e47cc07df4216ee5e987861de
MatteoMazzonelli <matteo.mazzonelli1@ibm.com>
2025-06-18 13:49:56 +0200

    Removed scripts

--- tests/test_yaml_configs.py
+++ tests/test_yaml_configs.py
@@ -4,7 +4,7 @@ import pytest
 from gridFM.io.param_handler import load_normalizer, get_loss_function, load_model, get_transform, NestedNamespace
 from gridFM.models.gps_transformer import GPSTransformer
 
-@pytest.mark.parametrize("yaml_path", glob.glob("scripts/config/*.yaml"))
+@pytest.mark.parametrize("yaml_path", glob.glob("examples/config/*.yaml"))
 def test_yaml_config_valid(yaml_path):
     with open(yaml_path) as f:
         config_dict = yaml.safe_load(f)
