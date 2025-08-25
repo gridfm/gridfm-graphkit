f87771e5c7dac92a5dfd48f8e924b8cc87f3db66
Celia Cintas <celia.cintas@ibm.com>
2025-08-25 13:58:07 +0300

    visualization functions

--- gridfm_graphkit/utils/visualization.py
+++ gridfm_graphkit/utils/visualization.py
@@ -2,6 +2,10 @@ import networkx as nx
 from gridfm_graphkit.training.loss import PBELoss
 from gridfm_graphkit.datasets.globals import PQ, PV, REF
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from scipy.stats import pearsonr
+import seaborn as sns
+import numpy as np
 
 
 def visualize_error(data_point, output, node_normalizer):
@@ -96,4 +100,196 @@ def visualize_error(data_point, output, node_normalizer):
 
     # Show plot
     plt.title("Nodal Active Power Residuals", fontsize=14, fontweight="bold")
+    plt.show()
+
+
+def plot_mass_correlation_density(true_vals, gfm_vals, model_name, label_plot):
+    """
+    TODO docstring
+
+    """
+    # TODO check if these parameters need to be passed by func or default behavior
+    vmin = 1
+    vmax = 5e6
+
+    x_min, x_max = 0, 2.25
+    y_min, y_max = 0, 3.0
+    bin_width = 0.01  # consistent bin width for both plots
+
+    # Generate consistent bins
+    x_bins = np.arange(x_min, x_max + bin_width, bin_width)
+    y_bins = np.arange(y_min, y_max + bin_width, bin_width)
+
+    # Pearson correlations
+    corr_gfm, _ = pearsonr(true_vals, gfm_vals)
+
+    # Create figure with shared x-axis
+    fig, ax1 = plt.subplots(figsize=(8, 6), dpi=400)
+
+    # --- GridFM Mass Correlation ---
+    h1 = ax1.hist2d(
+        true_vals,
+        gfm_vals,
+        bins=[x_bins, y_bins],
+        norm=LogNorm(vmin=vmin, vmax=vmax),
+        cmap="inferno",
+    )
+    ax1.axvline(1, color="black", linestyle="--", linewidth=2.0)
+    ax1.axhline(1, color="black", linestyle="--", linewidth=2.0)
+    ax1.plot([0, 5], [0, 5], "k--", linewidth=0.5)
+    ax1.set_xlabel("True Loadings", fontsize=12)
+    ax1.set_ylabel("Predicted Loadings", fontsize=12)
+    ax1.set_title(label_plot, fontsize=14)
+    ax1.text(
+        x_max - 1.5,
+        0.93,
+        f"r = {corr_gfm:.5f}",
+        transform=ax1.transAxes,
+        fontsize=13,
+        weight="bold",
+    )
+
+    # Colorbar
+    cbar = fig.colorbar(h1[3], ax=ax1, pad=0.02)
+    cbar.set_label("Number of samples", fontsize=10)
+
+    # Style adjustments
+    ax1.set_xlim(x_min, x_max)
+    ax1.set_ylim(y_min, y_max)
+    ax1.grid(True, linewidth=0.3)
+    ax1.tick_params(axis="both", labelsize=10)
+
+    plt.tight_layout()
+    plt.savefig(f"mass_correlation_density_{model_name}.png", bbox_inches="tight")
+    plt.show()
+
+
+def plot_cm(TN, FP, FN, TP, model_name, label_plot):
+    """
+    TODO docstring
+    """
+    cm = np.array([[TN, FP], [FN, TP]])
+
+    cm_labels = ["Non-overload", "Overload"]
+
+    fig_cm, ax_cm = plt.subplots(figsize=(6, 6))
+
+    sns.heatmap(
+        cm,
+        annot=True,
+        fmt="d",
+        cbar=False,
+        square=True,
+        linewidths=0.5,
+        cmap="Blues",
+        xticklabels=cm_labels,
+        yticklabels=cm_labels,
+        ax=ax_cm,
+        annot_kws={"size": 14},
+    )
+
+    ax_cm.set_xlabel("Predicted", fontsize=12)
+    ax_cm.set_ylabel("True", fontsize=12)
+    ax_cm.set_title(f"Confusion Matrix {label_plot}", fontsize=14)
+    ax_cm.tick_params(axis="both", labelsize=12)
+
+    plt.tight_layout()
+    plt.savefig(f"confusion_matrix_overload_{model_name}.png", bbox_inches="tight")
+    plt.show()
+
+
+def plot_loading_predictions(
+    loadings_pred,
+    loadings_dc,
+    loadings_gt,
+    prediction_dir,
+    label_plot,
+):
+    """
+    TODO docstrings
+    """
+    plt.hist(
+        loadings_pred,
+        alpha=0.5,
+        label=label_plot,
+        density=True,
+        bins=100,
+    )
+    plt.hist(loadings_dc, alpha=0.5, label="DC Solver", density=True, bins=100)
+    plt.hist(loadings_gt, alpha=0.5, label="Ground truth", density=True, bins=100)
+
+    plt.xlabel("Loading Values")
+    plt.ylabel("Density")
+    plt.yscale("log")
+    plt.legend()
+
+    plt.savefig(f"distribution_loading_predictions_{prediction_dir}.png")
+    plt.show()
+
+
+def plot_mass_correlation_density_voltage(pf_node, prediction_dir, label_plot):
+    """
+    TODO docstrings
+    TODO refactor if we pass by parameters a few more plot deets we can use plot_mass_correlation_density for both
+
+    """
+    # Get the global min and max for color scaling (avoid log(0) by setting min to at least 1)
+    vmin = 1
+    vmax = 5e6
+
+    x_min, x_max = 0.85, 1.15
+    y_min, y_max = 0.85, 1.15
+    bin_width = 0.001  # consistent bin width for both plots
+
+    # Generate consistent bins
+    x_bins = np.arange(x_min, x_max + bin_width, bin_width)
+    y_bins = np.arange(y_min, y_max + bin_width, bin_width)
+    # Pearson correlations
+    corr_vm, _ = pearsonr(pf_node["Vm"], pf_node["Vm_pred_corrected"])
+
+    # Create figure with shared x-axis
+    fig, ax1 = plt.subplots(figsize=(8, 6), dpi=400)
+
+    # --- GridFM Mass Correlation ---
+    h1 = ax1.hist2d(
+        pf_node["Vm"],
+        pf_node["Vm_pred_corrected"],
+        bins=[x_bins, y_bins],
+        norm=LogNorm(vmin=vmin, vmax=vmax),
+        cmap="inferno",
+    )
+    ax1.axvline(0.9, color="black", linestyle="--", linewidth=2.0)
+    ax1.axhline(0.9, color="black", linestyle="--", linewidth=2.0)
+    ax1.axvline(1.1, color="black", linestyle="--", linewidth=2.0)
+    ax1.axhline(1.1, color="black", linestyle="--", linewidth=2.0)
+    ax1.plot([0, 5], [0, 5], "k--", linewidth=0.5)
+    ax1.set_xlabel("True Voltage Magnitude", fontsize=12)
+    ax1.set_ylabel("Predicted Voltage magnitude", fontsize=12)
+    ax1.set_title(label_plot, fontsize=14)
+    ax1.text(
+        0.5,
+        0.95,
+        f"r = {corr_vm:.5f}",
+        transform=ax1.transAxes,
+        fontsize=13,
+        weight="bold",
+        ha="center",
+        va="top",
+    )
+
+    # Colorbar
+    cbar = fig.colorbar(h1[3], ax=ax1, pad=0.02)
+    cbar.set_label("Number of samples", fontsize=10)
+
+    # Style adjustments
+    ax1.set_xlim(x_min, x_max)
+    ax1.set_ylim(y_min, y_max)
+    ax1.grid(True, linewidth=0.3)
+    ax1.tick_params(axis="both", labelsize=10)
+
+    plt.tight_layout()
+    plt.savefig(
+        f"mass_correlation_density_voltage_{prediction_dir}.png",
+        bbox_inches="tight",
+    )
     plt.show()
