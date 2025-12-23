67daf106fc52a58753c39be9893c62c49201ca5a
Naomi Simumba <7224231+naomi-simumba@users.noreply.github.com>
2025-12-23 14:09:53 -0500

    reformat callbacks
    Signed-off-by: Naomi Simumba <7224231+naomi-simumba@users.noreply.github.com>

--- gridfm_graphkit/training/callbacks.py
+++ gridfm_graphkit/training/callbacks.py
@@ -1,6 +1,12 @@
 from lightning.pytorch.callbacks import Callback
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
 from lightning.pytorch.loggers import MLFlowLogger
+from lightning.pytorch.callbacks import (
+    Timer,
+    EarlyStopping,
+    ModelCheckpoint,
+    LearningRateMonitor,
+    )
 import os
 import torch
 
@@ -47,3 +53,44 @@ class SaveBestModelStateDict(Callback):
             # Save the model's state_dict
             model_path = os.path.join(model_dir, self.filename)
             torch.save(pl_module.state_dict(), model_path)
+
+
+
+
+def get_training_callbacks(callbacks):
+    #TODO: make monitored metric configurable 
+    callback_list = []
+    if callbacks.tol is not None and callbacks.patience is not None:
+        early_stop_callback = EarlyStopping(
+            monitor="Validation loss",
+            min_delta=callbacks.tol,
+            patience=callbacks.patience,
+            verbose=False,
+            mode="min",
+        )
+        callback_list.append(early_stop_callback)
+
+    save_best_model_callback = SaveBestModelStateDict(
+        monitor="Validation loss",
+        mode="min",
+        filename="best_model_state_dict.pt",
+    )
+    callback_list.append(save_best_model_callback)
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="Validation loss",  # or whichever metric you track
+        mode="min",
+        save_last=True,
+        save_top_k=0,
+    )
+    callback_list.append(checkpoint_callback)
+
+    if callbacks.max_run_duration is not None:
+        max_run_callback = Timer(duration=callbacks.max_run_duration)
+        callback_list.append(max_run_callback)
+
+    if callbacks.monitor_learning_rate:
+        learning_rate_callback = LearningRateMonitor(logging_interval="epoch")
+        callback_list.append(learning_rate_callback)
+
+    return callback_list
