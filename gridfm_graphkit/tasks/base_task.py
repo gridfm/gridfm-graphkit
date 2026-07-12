ea3086a3ababde75dbd291e930d4e106764fa03d
Alban Puech <alban.puech2@ibm.com>
2026-07-12 12:24:05 -0400

    import LR_SCHEDULER_MONITOR

--- gridfm_graphkit/tasks/base_task.py
+++ gridfm_graphkit/tasks/base_task.py
@@ -7,7 +7,7 @@ from lightning.pytorch.loggers import MLFlowLogger
 import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from gridfm_graphkit.training.callbacks import BEST_CHECKPOINT_MONITOR
+from gridfm_graphkit.training.callbacks import LR_SCHEDULER_MONITOR
 
 
 class BaseTask(L.LightningModule, ABC):
@@ -129,7 +129,7 @@ class BaseTask(L.LightningModule, ABC):
             "optimizer": self.optimizer,
             "lr_scheduler": {
                 "scheduler": self.scheduler,
-                "monitor": BEST_CHECKPOINT_MONITOR,
+                "monitor": LR_SCHEDULER_MONITOR,
                 "reduce_on_plateau": True,
             },
         }
